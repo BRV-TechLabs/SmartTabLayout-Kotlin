c654d8bce054acafd8a8e3b13f810b4e8584c92e
Benjoe Vidal <benjoe22@gmail.com>
2020-07-07 18:54:18 +0800

    Added SmartTabLayout

--- app/src/main/java/com/vidalbenjoe/ph/android/smarttablayout/MainActivity.kt
+++ app/src/main/java/com/vidalbenjoe/ph/android/smarttablayout/MainActivity.kt
@@ -2,10 +2,31 @@ package com.vidalbenjoe.ph.android.smarttablayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems
+import com.vidalbenjoe.ph.android.smarttablayout.fragments.FirstFragment
+import com.vidalbenjoe.ph.android.smarttablayout.fragments.FourthFragment
+import com.vidalbenjoe.ph.android.smarttablayout.fragments.SecondFragment
+import com.vidalbenjoe.ph.android.smarttablayout.fragments.ThirdFragment
+import kotlinx.android.synthetic.main.main_tab_layout.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.main_tab_layout)
+
+
+        val adapter = FragmentPagerItemAdapter(
+            supportFragmentManager, FragmentPagerItems.with(this)
+                .add("First Fragment", FirstFragment::class.java)
+                .add("Second Fragment", SecondFragment::class.java)
+                .add("Third Fragment", ThirdFragment::class.java)
+                .add("Fourth Fragment", FourthFragment::class.java)
+                .create()
+        )
+
+        viewPager.adapter = adapter
+        viewPagerTab.setViewPager(viewPager)
+
     }
 }
---
+++ app/src/main/java/com/vidalbenjoe/ph/android/smarttablayout/fragments/FirstFragment.kt
@@ -0,0 +1,23 @@
+package com.vidalbenjoe.ph.android.smarttablayout.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.Nullable
+import androidx.fragment.app.Fragment
+import com.vidalbenjoe.ph.android.smarttablayout.R
+
+class FirstFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, @Nullable container: ViewGroup?,
+        @Nullable savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_one_layout, container, false)
+    }
+
+     override fun onViewCreated(view: View, @Nullable savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
